e8878ab44ebdcd75f78aa0ef7fdc8a5ab837d7fb
Geradot <toshkageradot@tuta.io>
2023-07-31 17:31:20 -0500

    Поправление gutter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className="overscroll-y-none">
+    <Html lang="en">
       <Head />
       <body className="bg-Black">
         <Main />
