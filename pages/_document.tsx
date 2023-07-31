9b9706470a91e29d6dd309a291e3084dd0ede96e
Geradot <toshkageradot@tuta.io>
2023-07-31 17:22:52 -0500

    Мелкие исправления

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="overscroll-y-none">
       <Head />
       <body className="bg-Black">
         <Main />
